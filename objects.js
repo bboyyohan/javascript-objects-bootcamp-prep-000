47d82fdaffc4d150074f72d0a13a11ab89d433a6
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-10-01 17:32:47 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,10 @@
-var playlist = new Object({keys:'0'}); 
+var playlist = {artistName: `songTitle`};
 
-function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
+function updatePlaylist (playlist, artistName, songTitle) {
+  return playlist[artistName] = `songTitle`;
 }
+
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
-  return playlist
+  return playlist;
 }
